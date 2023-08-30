b7329a4fafbfe19012aa7dd78c01b6b1325a7ea5
MBARUSHIMANA Fabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2023-08-30 15:35:48 +0200

    Update Function.js
    add show and hide  navigation.  on  (max-width: 768px) automatically without  reflash page.

--- Navigation version2/Function.js
+++ Navigation version2/Function.js
@@ -1,6 +1,11 @@
 let button= document.getElementById("logo")
 let navigation = document.querySelector(".navigation")
 let btncross = document.getElementById("cross")
+// add show icon 
+
+let sidebar=document.getElementById("sidebar");
+
+
 console.log(navigation);
 button .onclick = () =>{
     navigation.style.display ="block"
@@ -8,4 +13,39 @@ button .onclick = () =>{
 
 btncross .onclick = () =>{
     navigation.style.display ="none"
+    sidebar.style.display="flex";
+    
 }
+
+
+
+// new change
+
+function handleMediaChange(mediaQuery) {
+    if (mediaQuery.matches) {
+        navigation.style.display ="none"
+        sidebar.style.display="flex";
+    } else {
+        navigation.style.display ="flex"
+        sidebar.style.display="none";
+    }
+  }
+  const mediaQuery = window.matchMedia("(max-width: 768px)");
+  
+   handleMediaChange(mediaQuery);
+   mediaQuery.addEventListener("change", handleMediaChange);
+  
+   window.addEventListener("resize", () => {
+    handleMediaChange(mediaQuery);
+  });
+  
+
+
+//   show navigation
+
+sidebar.addEventListener("click",()=>{
+    navigation.style.display ="block"
+    sidebar.style.display="none";
+})
+
+ 
